3de7ac8e9f5b4b6b3fff92abb1dd7125723065e7
bb_golden <itsgolden235@gmail.com>
2025-05-01 22:56:11 -0500

    added text

--- app/(tabs)/run.tsx
+++ app/(tabs)/run.tsx
@@ -1,9 +1,11 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function StartRunScreen() {
     return (
         <View>
-
+            <Text>
+                this is text
+            </Text>
         </View>
     )
 }
