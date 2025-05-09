940f01ff5b5a1cbcbb63eb7a00edac3ad725334e
bbgolden <itsgolden235@gmail.com>
2025-05-09 10:10:06 -0500

    paper mache index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,38 @@
 import { Href, Link } from "expo-router";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+
+const deviceHeight = Dimensions.get("window").height;
+const deviceWidth = Dimensions.get("window").width;
 
 export default function AccountScreen() {
   return (
     <View style={styles.container}>
-      <Image/>
-      <Text>
-        username
+
+      <Image
+        source={{ uri: "https://blog.logrocket.com/wp-content/uploads/2023/01/set-border-radius-inner-view-1.png" }}
+        style={styles.profilePicture}
+      />
+
+      <Text style={styles.header}>
+        account name
       </Text>
-      <Link href={"/log" as Href }>
-        go to log
+
+      <Link href={"/list" as Href}>
+        <View style={styles.previewBox}>
+          <Text>
+            last run box
+          </Text>
+        </View>
       </Link>
+
+      <Text style={styles.header}>
+        Overall Stats
+      </Text>
+
+      <View style={styles.statsBox}>
+
+      </View>
+
     </View>
   );
 }
@@ -19,5 +41,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#545454",
+    alignItems: "center",
+  },
+  profilePicture: {
+    height: deviceHeight / 10,
+    width: deviceHeight / 10,
+    borderRadius: 60,
+    borderWidth: 3,
+    borderColor: "#ca4d0b",
+  },
+  header: {
+    fontSize: deviceHeight / 40,
+    color: "#ca4d0b",
+    fontWeight: "bold",
+  },
+  previewBox: {
+    height: deviceHeight / 4,
+    width: deviceWidth * 4 / 5,
+    borderWidth: 2,
+    borderColor: "#ec690d",
+  },
+  statsBox: {
+    height: deviceHeight / 3,
+    width: deviceWidth,
+    alignItems: "center",
+    justifyContent: "center",
   }
 });
